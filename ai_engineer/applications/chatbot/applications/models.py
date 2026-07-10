98ebf8ed308ab2b14a7fc23cae027f8f25343319
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-07-10 14:51:43 +0700

    add caller service

---
+++ ai_engineer/applications/chatbot/applications/models.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class LLMResponse(BaseModel):
+    content: str
--- test_chat_witl_llm.py
+++ test_chat_witl_llm.py
@@ -19,57 +19,7 @@ llm = LLMCallerService(
     temperature=temperature,
 )
 
-# content, response_metadata, tool_calls, invalid_tool_calls, usage_metadata = llm.call_llm(
-#     prompt="Hello world!"
-# )
-# print(content)
-# print(response_metadata)
-# print(tool_calls)
-# print(invalid_tool_calls)
-# print(usage_metadata)
-
-# template = [
-#     {
-#         "role": "system",
-#         "content": (
-#             "[PERSONA] You are a financial expert. Your expertise spans stock analysis, account management, and financial analysis (including monetary policy and public investment).\n"
-#             "[TASK] Communicate with user. Help them answer question about stock market.\n"
-#             "[CONTEXT] You operate within a bank like Techcombank or Viettinbank. Think and respond like a financial expert.\n"
-#             "[FORMAT] Structure your answer as follows:\n"
-#             "Output users in text format\n"
-#             "\n"
-#             "{format_instructions}\n"
-#             "\n"
-#         ),
-#     },
-#     {
-#         "role": "user",
-#         "content": (
-#             "Help me answer this question with the context I send to you. If context is None you can skip this context and focus only about the question.\n\n"
-#             "question: {question}\n"
-#             "question_context: {question_context}\n"
-#             '"""'
-#         ),
-#     },
-# ]
-
-
-class LLMResponse(BaseModel):
-    content: str
-   
-template = ChatbotPromptLoading().load_and_parse_prompt()
-
-parser = PydanticOutputParser(pydantic_object=LLMResponse)
-prompt = template.partial(format_instructions=parser.get_format_instructions())
-
-
-chain = prompt | llm.llm | parser
-
-result = chain.invoke(
-    {
-        "question": "Ronaldo la ai!",
-        "question_context": None,
-    }
+response = llm.call_llm(
+    user_question="Hello world!",
 )
-
-print(result)
+print(response)
